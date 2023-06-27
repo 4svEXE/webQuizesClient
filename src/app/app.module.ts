fab581ae2f8227e5fe9fc9182bb1b282a0b707c7
Иван Сташко <4sv.exe@gmail.com>
2023-06-27 19:19:54 +0300

    add tostes to new Test question page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +24,9 @@ import { CreateTestComponent } from './views/pages/create-test/create-test.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/pages/create-test/create-test.component.ts
+++ src/app/views/pages/create-test/create-test.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { TestModel } from "src/app/models/Test";
+import { NotificationService } from "src/app/services/notification.service";
 import { TestService } from "src/app/services/test.service";
 
 @Component({
@@ -9,31 +10,74 @@ import { TestService } from "src/app/services/test.service";
 })
 export class CreateTestComponent {
   testModel: TestModel = new TestModel(
-    "empty", // title
+    "", // title
     1, // difficulty
     [
-      { title: "string", isCorrect: true },
-      { title: "string", isCorrect: false },
-      { title: "string", isCorrect: false },
-      { title: "string", isCorrect: false },
+      { title: "", isCorrect: true },
+      { title: "", isCorrect: false },
+      { title: "", isCorrect: false },
+      { title: "", isCorrect: false },
     ], // answers
-    "string", // image
-    "string" // explanation
+    "", // image
+    "" // explanation
   );
 
-  constructor(private testService: TestService) {}
+  constructor(
+    private testService: TestService,
+    private notifyService: NotificationService
+  ) {}
 
-  createNewTest() {
-    
+  createNewTest(event: any) {
+    event.preventDefault();
+
     this.testService
       .createTestQuestion(this.testModel)
       .subscribe((responce: any) => {
-        console.log("res", responce);
+        this.showToasterSuccess(
+          "Test question wos created successfully!",
+          "Succes!"
+        );
+
+        console.log("responce", responce);
+        this.clearInputs();
       });
   }
 
   setDifficulty(difficulty: number) {
     this.testModel.difficulty = difficulty;
+  }
+
+  clearInputs() {
+    this.testModel = new TestModel(
+      "", // title
+      1, // difficulty
+      [
+        { title: "", isCorrect: true },
+        { title: "", isCorrect: false },
+        { title: "", isCorrect: false },
+        { title: "", isCorrect: false },
+      ], // answers
+      "", // image
+      "" // explanation
+    );
+  }
+
+  // Notifications
+
+  showToasterSuccess(message: string, title: string) {
+    this.notifyService.showSuccess(message, title);
+  }
+
+  showToasterError(message: string, title: string) {
+    this.notifyService.showError(message, title);
+  }
+
+  showToasterInfo(message: string, title: string) {
+    this.notifyService.showInfo(message, title);
+  }
+
+  showToasterWarning(message: string, title: string) {
+    this.notifyService.showWarning(message, title);
   }
 }
 
